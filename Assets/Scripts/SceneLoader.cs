55bdf6edca604539a380d2d248afecfd9702e746
JonathanVanDyke <jfv213@gmail.com>
2020-05-30 12:27:19 -0400

    Organize scene and project folder

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,27 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
-    private void Awake()
-    {
-        DontDestroyOnLoad(this.gameObject);
-    }
-
-
     void Start()
     {
         Invoke("LoadFirstScene", 5f);
-    }
-
-
-    void Update()
-    {
-        
     }
 
     private void LoadFirstScene()
     {
         SceneManager.LoadScene(1);
     }
+
 }
